2fc166e881362f85972751a56ce54d4b1a0059b5
Bames007 <eddybames007@gmail.com>
2022-08-10 14:41:19 +0100

    New Planet 99

---
+++ lib/screens/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class newClass extends StatefulWidget {
+  const newClass({Key? key}) : super(key: key);
+
+  @override
+  State<newClass> createState() => _newClassState();
+}
+
+class _newClassState extends State<newClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
